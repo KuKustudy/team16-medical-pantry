3686e0a0bcf9921fcc1599c7f9b93116d307781b
Biscuit4434 <ansongao4434@gmail.com>
2025-09-04 08:19:57 +1000

    Page setup

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,9 +1,25 @@
-import Header from "./Header"
+import { HashRouter, Routes, Route, Link } from "react-router-dom";
+import { Home } from "./Pages/Home";
+import { Page1 } from "./Pages/Page1";
+import { Page2 } from "./Pages/Page2";
+import { Page3 } from "./Pages/Page3";
 
-function App() {
+export default function App() {
   return (
-    <Header></Header>
+    <HashRouter>
+      <nav style={{display:"flex", gap:12, padding:12}}>
+        <Link to="/">Home</Link>
+        <Link to="/page1">Page1</Link>
+        <Link to="/page2">Page2</Link>
+        <Link to="/page3">Page3</Link>
+      </nav>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/page1" element={<Page1 />} />
+        <Route path="/page2" element={<Page2 />} />
+        <Route path="/page3" element={<Page3 />} />
+        <Route path="*" element={<Home />} />
+      </Routes>
+    </HashRouter>
   );
 }
-
-export default App
