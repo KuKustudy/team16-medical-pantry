fe1a6d25747c45c8bce530c17ded682a9d7260d3
PseudoCav <jess.sin@student.unimelb.edu.au>
2025-09-07 13:12:59 +1000

    mongodb initialisation

--- backend/index.js
+++ backend/index.js
@@ -18,3 +18,29 @@ app.get("/api", (req, res) =>{
 app.listen(8080, () => {
     console.log("Server started on port 8080");
 });
+
+//mongodb database access
+//mongodb+srv://jesssin_db_user:PrPpU2xltmLPwNr2@medical-data.3tzehjr.mongodb.net/?retryWrites=true&w=majority&appName=Medical-Data
+/*const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = "mongodb+srv://jesssin_db_user:PrPpU2xltmLPwNr2@medical-data.3tzehjr.mongodb.net/?retryWrites=true&w=majority&appName=Medical-Data";
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+}
+run().catch(console.dir);*/
