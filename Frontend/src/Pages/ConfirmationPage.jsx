bc9e374287537754e56a2aa2be71d8897cdab00d
Biscuit4434 <ansongao4434@gmail.com>
2025-09-14 10:16:06 +1000

    doing the confirmation page

--- Frontend/src/Pages/ConfirmationPage.jsx
+++ Frontend/src/Pages/ConfirmationPage.jsx
@@ -1,10 +1,12 @@
 import React, {useState} from "react";
 import { Header } from "../Components/Header";
+import MedicalInput from "../Components/MedicalInput";
 
 export function ConfirmationPage(){
     return (
         <>
             <Header />
+            <MedicalInput />
         </>
     );
 }
