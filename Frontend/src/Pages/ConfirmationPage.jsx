54f302dd95fdcadab446e8c0fb6b9c6bdcb8334a
Wingyee_He <136226807+KuKustudy@users.noreply.github.com>
2025-09-27 23:10:24 +1000

    navigate to confirmation page after scanned text and prefill textarea

--- Frontend/src/Pages/ConfirmationPage.jsx
+++ Frontend/src/Pages/ConfirmationPage.jsx
@@ -1,12 +1,20 @@
 import React, {useState} from "react";
+import { useLocation } from "react-router-dom";
 import { Header } from "../Components/Header";
 import MedicalInput from "../Components/MedicalInput";
 
+
 export function ConfirmationPage(){
+    // location is a hook that holds the URI of the current page
+    // location.state will hold the scanned text when scan page send the scanned text
+    const location = useLocation();
+    const { scannedText } = location.state || {};
+    console.log("received item name", scannedText);
+
     return (
         <>
             <Header />
-            <MedicalInput />
+            <MedicalInput initialItemName={scannedText} />
         </>
     );
 }
