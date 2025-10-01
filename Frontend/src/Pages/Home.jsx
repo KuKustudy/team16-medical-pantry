b53f9f19cf63262ad970b97c9473285153f5e0a0
Biscuit4434 <ansongao4434@gmail.com>
2025-10-02 00:53:03 +1000

    Polished up the optionsPage. Added Rescan button. Added buttons to home page

--- Frontend/src/Pages/Home.jsx
+++ Frontend/src/Pages/Home.jsx
@@ -1,10 +1,11 @@
 import { Header } from "../Components/Header";
+import HomeButton from "../Components/HomeButton";
 
-export function Home(){
-    return (
-        <>
-            <Header />
-            
-        </>
-    );
+export function Home() {
+  return (
+    <>
+      <Header />
+      <HomeButton />
+    </>
+  );
 }
