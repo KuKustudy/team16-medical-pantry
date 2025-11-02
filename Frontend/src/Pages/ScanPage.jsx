471848d8316c874ddba96d3e028ba7f9e70b3f60
Biscuit4434 <ansongao4434@gmail.com>
2025-11-02 13:15:27 +1100

    Added header to scanpage, fixed scrolling on optionspage

--- Frontend/src/Pages/ScanPage.jsx
+++ Frontend/src/Pages/ScanPage.jsx
@@ -1,9 +1,11 @@
 import React from "react";
 import SimpleCam from "../Components/SimpleCam";
+import { Header } from "../Components/Header";
 
 export function ScanPage() {
   return (
     <>
+    <Header />
       <main style={{ padding: 0, margin: 0 }}>
         <SimpleCam />
       </main>
